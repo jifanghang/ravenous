63a984683b4f3ab0f59690ecd84509904f306dc7
Jamie Ji <jifanghang@gmail.com>
2019-01-09 20:48:39 +0100

    [MOD]change business component structure using props

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,10 +6,11 @@ class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+        {
+          this.props.businesses.map((business) => {
+            return <business business={business} />
+          })
+        }
       </div>
     );
   }	
