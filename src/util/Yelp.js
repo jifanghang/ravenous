d9e19711510e1cbcdcfc6dbb176a7c06d608ea51
jifanghang <jifanghang@gmail.com>
2019-01-14 14:32:19 +0100

    [ADD]finished Yelp.js

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,7 +1,38 @@
 const apiKey = 't-uaMyIxJCiJFCOXxsRKzez5cF15YY5Y7FXCqNAsFQf2k7CdtUazj5qKrM1EHaTZ_5nXeWPQftWwdeC-4qku_jY-E9RwhV7DkwxGKXpd6hvdUXEPgamerK1pVOI7XHYx';
 
-Yelp = {};
+const Yelp = {
+  function search(term, location, sortBy) {
+    // return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+    //              {
+    //                 headers: {`Bearer ${apiKey}`}
+    //               });
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, { 
+      headers: {
+        Authorization: `Bearer ${apiKey}` 
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if(jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => {
+          console.log(business);
+          return {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count
+          };
+        })
+      }
+    });
+  }
 
-function search(term, location, sortBy) {
-  return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
-} 
+};
+
+export default Yelp;
