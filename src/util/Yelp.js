f5ed9ba74c03e0645e72ed26b5d742398a6c5083
Jamie <jifanghang@gmail.com>
2019-01-14 02:23:39 +0100

    [ADD]Yelp.js

---
+++ src/util/Yelp.js
@@ -0,0 +1,7 @@
+const apiKey = 't-uaMyIxJCiJFCOXxsRKzez5cF15YY5Y7FXCqNAsFQf2k7CdtUazj5qKrM1EHaTZ_5nXeWPQftWwdeC-4qku_jY-E9RwhV7DkwxGKXpd6hvdUXEPgamerK1pVOI7XHYx';
+
+Yelp = {};
+
+function search(term, location, sortBy) {
+  return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
+}
